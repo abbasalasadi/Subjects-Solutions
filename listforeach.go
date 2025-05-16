6c94a070b48c69059335eb96aac8235dc584f185
abbasalasadi <abbasalasadi@gmail.com>
2025-05-16 19:38:50 +0300

    sd

---
+++ listforeach.go
@@ -0,0 +1,52 @@
+package piscine
+
+type NodeL struct {
+	Data interface{}
+	Next *NodeL
+}
+
+type List struct {
+	Head *NodeL
+	Tail *NodeL
+}
+
+func ListPushBack(l *List, data interface{}) {
+	NewNode := &NodeL{Data: data}
+	if l.Tail == nil {
+		l.Head = NewNode
+		l.Tail = NewNode
+	} else {
+		l.Tail.Next = NewNode
+		l.Tail = NewNode
+	}
+}
+
+func Add2_node(node *NodeL) {
+	switch node.Data.(type) {
+	case int:
+		node.Data = node.Data.(int) + 2
+	case string:
+		node.Data = node.Data.(string) + "2"
+	}
+}
+
+func Subtract3_node(node *NodeL) {
+	switch node.Data.(type) {
+	case int:
+		node.Data = node.Data.(int) - 3
+	case string:
+		node.Data = node.Data.(string) + "-3"
+	}
+}
+
+func ListForEach(l *List, f func(*NodeL)) {
+	if l == nil || l.Head == nil {
+		return
+	}
+
+	current := l.Head
+	for current != nil {
+		f(current)
+		current = current.Next
+	}
+}
--- listreverse.go
+++ listreverse.go
@@ -1,25 +1,25 @@
 package piscine
 
-type NodeL struct {
-	Data interface{}
-	Next *NodeL
-}
+// type NodeL struct {
+// 	Data interface{}
+// 	Next *NodeL
+// }
 
-type List struct {
-	Head *NodeL
-	Tail *NodeL
-}
+// type List struct {
+// 	Head *NodeL
+// 	Tail *NodeL
+// }
 
-func ListPushBack(l *List, data interface{}) {
-	NewNode := &NodeL{Data: data}
-	if l.Tail == nil {
-		l.Head = NewNode
-		l.Tail = NewNode
-	} else {
-		l.Tail.Next = NewNode
-		l.Tail = NewNode
-	}
-}
+// func ListPushBack(l *List, data interface{}) {
+// 	NewNode := &NodeL{Data: data}
+// 	if l.Tail == nil {
+// 		l.Head = NewNode
+// 		l.Tail = NewNode
+// 	} else {
+// 		l.Tail.Next = NewNode
+// 		l.Tail = NewNode
+// 	}
+// }
 
 func ListReverse(l *List) {
 	if l == nil || l.Head == nil {
