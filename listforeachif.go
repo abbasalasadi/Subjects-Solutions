b67373b1dbbfdb37306edc0d4f2854ddc5ae47a6
abbasalasadi <abbasalasadi@gmail.com>
2025-05-16 19:57:36 +0300

    sd

---
+++ listforeachif.go
@@ -0,0 +1,53 @@
+package piscine
+
+type NodeL struct {
+	Data interface{}
+	Next *NodeL
+}
+
+type List struct {
+	Head *NodeL
+	Tail *NodeL
+}
+
+func ListPushBack(l *List, data interface{}) {
+	NewNode := &NodeL{Data: data}
+	if l.Tail == nil {
+		l.Head = NewNode
+		l.Tail = NewNode
+	} else {
+		l.Tail.Next = NewNode
+		l.Tail = NewNode
+	}
+}
+
+func IsPositiveNode(node *NodeL) bool {
+	switch node.Data.(type) {
+	case int, float32, float64, byte:
+		return node.Data.(int) > 0
+	default:
+		return false
+	}
+}
+
+func IsAlNode(node *NodeL) bool {
+	switch node.Data.(type) {
+	case int, float32, float64, byte:
+		return false
+	default:
+		return true
+	}
+}
+
+func ListForEachIf(l *List, f func(*NodeL), cond func(*NodeL) bool) {
+	if l == nil || f == nil || cond == nil {
+		return
+	}
+	current := l.Head
+	for current != nil {
+		if cond(current) {
+			f(current)
+		}
+		current = current.Next
+	}
+}
