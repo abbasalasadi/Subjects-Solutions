503133c3b41c1ed2a7a412a36b08814330341023
abbasalasadi <abbasalasadi@gmail.com>
2025-05-16 17:05:57 +0300

    sd

---
+++ listpushfront.go
@@ -0,0 +1,23 @@
+package piscine
+
+type NodeL struct {
+	Data interface{}
+	Next *NodeL
+}
+
+type List struct {
+	Head *NodeL
+	Tail *NodeL
+}
+
+func ListPushFront(l *List, data interface{}) {
+	NewNode := &NodeL{Data: data}
+
+	if l.Head == nil {
+		l.Head = NewNode
+		l.Tail = NewNode
+	} else {
+		NewNode.Next = l.Head
+		l.Head = NewNode
+	}
+}
