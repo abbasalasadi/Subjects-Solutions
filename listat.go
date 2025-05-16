3a965f4097eb7e2ba9d21ccb5e0c853cef3eb0c3
abbasalasadi <abbasalasadi@gmail.com>
2025-05-16 19:29:13 +0300

    sd

--- listat.go
+++ listat.go
@@ -2,26 +2,26 @@ package piscine
 
 // import "fmt"
 
-type NodeL struct {
-	Data interface{}
-	Next *NodeL
-}
+// type NodeL struct {
+// 	Data interface{}
+// 	Next *NodeL
+// }
 
-type List struct {
-	Head *NodeL
-	Tail *NodeL
-}
+// type List struct {
+// 	Head *NodeL
+// 	Tail *NodeL
+// }
 
-func ListPushBack(l *List, data interface{}) {
-	NewNode := &NodeL{Data: data}
-	if l.Tail == nil {
-		l.Head = NewNode
-		l.Tail = NewNode
-	} else {
-		l.Tail.Next = NewNode
-		l.Tail = NewNode
-	}
-}
+// func ListPushBack(l *List, data interface{}) {
+// 	NewNode := &NodeL{Data: data}
+// 	if l.Tail == nil {
+// 		l.Head = NewNode
+// 		l.Tail = NewNode
+// 	} else {
+// 		l.Tail.Next = NewNode
+// 		l.Tail = NewNode
+// 	}
+// }
 
 func ListSize(l *List) int {
 	if l.Head == nil {
---
+++ listreverse.go
@@ -0,0 +1,39 @@
+package piscine
+
+type NodeL struct {
+	Data interface{}
+	Next *NodeL
+}
+
+type List struct {
+	Head *NodeL
+	Tail *NodeL
+}
+
+func ListPushBack(l *List, data interface{}) {
+	NewNode := &NodeL{Data: data}
+	if l.Tail == nil {
+		l.Head = NewNode
+		l.Tail = NewNode
+	} else {
+		l.Tail.Next = NewNode
+		l.Tail = NewNode
+	}
+}
+
+func ListReverse(l *List) {
+	if l == nil || l.Head == nil {
+		return
+	}
+
+	var prev *NodeL
+	current := l.Head
+	l.Tail = l.Head
+	for current != nil {
+		next := current.Next
+		current.Next = prev
+		prev = current
+		current = next
+	}
+	l.Head = prev
+}
