f0dc0f799fd538d0bf6b96334266c53364d4b0d0
abbasalasadi <abbasalasadi@gmail.com>
2025-05-18 02:23:17 +0300

    sd

--- btreeapplypreorder.go
+++ btreeapplypreorder.go
@@ -1,23 +1,23 @@
 package piscine
 
-type TreeNode struct {
-	Left, Right, Parent *TreeNode
-	Data                string
-}
+// type TreeNode struct {
+// 	Left, Right, Parent *TreeNode
+// 	Data                string
+// }
 
-func BTreeInsertData(root *TreeNode, data string) *TreeNode {
-	if root == nil {
-		return &TreeNode{Data: data}
-	}
-	if data < root.Data {
-		root.Left = BTreeInsertData(root.Left, data)
-		root.Left.Parent = root
-	} else {
-		root.Right = BTreeInsertData(root.Right, data)
-		root.Right.Parent = root
-	}
-	return root
-}
+// func BTreeInsertData(root *TreeNode, data string) *TreeNode {
+// 	if root == nil {
+// 		return &TreeNode{Data: data}
+// 	}
+// 	if data < root.Data {
+// 		root.Left = BTreeInsertData(root.Left, data)
+// 		root.Left.Parent = root
+// 	} else {
+// 		root.Right = BTreeInsertData(root.Right, data)
+// 		root.Right.Parent = root
+// 	}
+// 	return root
+// }
 
 func BTreeApplyPreorder(root *TreeNode, f func(...interface{}) (int, error)) {
 	if root == nil {
