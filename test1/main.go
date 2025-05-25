fada47105f7e8c7b69ea0d9b3a1f2cd203eac63e
abbasalasadi <abbasalasadi@gmail.com>
2025-05-25 13:46:20 +0300

    sd

--- test1/main.go
+++ test1/main.go
@@ -1,10 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	// "math"
+)
 
-func main() {
-a,err:=fmt.Println()
-fmt.Println(a)
-fmt.Println(err)
+type parameters interface{
+	Area() float64
+	Circomference() float64
+}
 
+type Rictangle struct{
+	ShortSide float64
+	LongSide float64
+}
+
+func (r Rictangle) Area() float64 {
+	return r.LongSide * r.ShortSide
+}
+
+func (r Rictangle) Circomference() float64{
+	return 2*(r.LongSide+r.ShortSide)
+}
+
+func main(){
+	R:=Rictangle{ShortSide: 6, LongSide: 8}
+	fmt.Printf("Rectangle L: %v, S: %v, Area: %v, Circomference: %v\n", R.LongSide, R.ShortSide, R.Area(), R.Circomference())
 }
